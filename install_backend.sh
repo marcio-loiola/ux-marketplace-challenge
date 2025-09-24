584df420f10fa6ef4bc5daad7aabdff71a6065d6
Marcio <marciogomes@alu.ufc.br>
2025-09-23 23:39:34 -0300

    fix(structure): Corrige a estrutura de pastas do backend, removendo a pasta aninhada

--- install_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "--- Changing directory to backend ---"
-cd backend
-echo "--- Current directory: $(pwd) ---"
-echo "--- Installing npm dependencies ---"
-npm install
-echo "--- npm install complete ---"
